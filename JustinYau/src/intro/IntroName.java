370ade21e09ad09a3bd2a021c29bc16beab281e8
Justin Yau <justiceman8998@gmail.com>
2017-09-12 10:53:28 -0400

    Added comments.

--- JustinYau/src/intro/IntroName.java
+++ JustinYau/src/intro/IntroName.java
@@ -1,10 +1,12 @@
+//This is a package declaration
 package intro;
 
+//This is a class declaration
 public class IntroName {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
