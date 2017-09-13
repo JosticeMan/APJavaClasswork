a4cb4286654e33b82a52afd0b234dcad17e07e0c
BT_1N3_17 <BT_1N3_17@BT_1N3_17.bktech.local>
2017-09-13 10:57:46 -0400

    Added function w/ Notes

--- JustinYau/src/intro/CodingConventions.java
+++ JustinYau/src/intro/CodingConventions.java
@@ -7,18 +7,29 @@ public class CodingConventions {
 	//This is a special method, a "constructor". 
 	//Notice that it does not have a return type. 
 	//That is because it returns an instance of the class. 
-	*Therefore, the name of the class must match the name of the constructor. 
+	* Therefore, the name of the class must match the name of the constructor. 
 	*/
 	
 	//fields
 	private String name;
 	private String description;
 	
-	public CodingConventions()
-	{
+	public CodingConventions(String name, int index){
 		//In Java, fields are instantiated in a constructor
-		name = "Justin Yau";
-		description = "Is a student";
+		//Adding ( this ) to a variable allows the function to access the value of the global variable when there is a local variable with the same 
+		// handle. 
+		this.name = name;
+		//Static reference begins with a class name. 
+		description = IntroName.DESCRIPTIONS[index];
+	}
+
+	public void doStuff() {
+		//Static Method Call rather than an Instance Method Call.
+		//Static Method Call is always there. (In this case, Math and System are Static Method Calls)
+		//System is capitalized. Calling a field inside of System. 
+		//A capital letter references a class. 
+		System.out.println(name + description); //Use a space between operations so like 1 + 1 ; It keeps things organized.
+		System.out.println("The square root of 12 is "+ Math.sqrt(12) + ".");
 	}
 	
 }
--- JustinYau/src/intro/IntroName.java
+++ JustinYau/src/intro/IntroName.java
@@ -4,13 +4,22 @@ package intro;
 //This is a class declaration
 public class IntroName {
 
+	//Constants (Public)
+	public static final String[] DESCRIPTIONS = {" is a student."
+	                                            ," is a teacher."
+	                                            ," is a poodle."};
+	 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		//Declaration of a local variable
 		//Notice the use of the word "new"
 		//New must be used to call a constructor
-		CodingConventions conventionsInstance = new CodingConventions();
+		CodingConventions conventionsInstance = new CodingConventions("Justin Yau", 0);
+		
+		//ConventionsInstance is an individual instance of CodingConventions
+		//Instance Method Call
+		conventionsInstance.doStuff();
 	}
 
 }
