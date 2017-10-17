5d9a0aa79beb06654ea15e8db101f39ad003489c
BT_1N3_17 <BT_1N3_17@BT_1N3_17.bktech.local>
2017-10-17 11:36:21 -0400

    Added classwork from the 17th

--- JustinYau/src/arrays/ArraysMain.java
+++ JustinYau/src/arrays/ArraysMain.java
@@ -9,14 +9,112 @@ public class ArraysMain {
 	
 	public ArraysMain() {
 		intRay = new int[100];
-		//populate(intRay);
+		populate(intRay);
+		System.out.println("The longest consecutive sequence occured on the " + (longestConsecSeqAndPos(intRay)[1]+1) + "th roll with a length of " + (longestConsecSeqAndPos(intRay)[0]) +  ". Starting with a roll of " + intRay[longestConsecSeqAndPos(intRay)[1]] + ".");
 		//checkOccurences(intRay, 3, 18);
-		populate1ToN(intRay);
-		shuffle(intRay);
+		//populate1ToN(intRay);
+		//shuffle(intRay);
+		//System.out.println(Arrays.toString(reverseOrder(intRay)));
+		//System.out.println(countLessThan(intRay, 10));
 		//Arrays is a Utility class included in Java formatting output
 		System.out.println(Arrays.toString(intRay));
 	}
 
+	//---------CLASSWORK 10/17/2017 --------------
+	
+	private int[] reverseOrder(int[] arr) {
+		int[] newIntRay = new int[arr.length];
+		for(int i = 0; i < arr.length; i++) {
+			newIntRay[i] = arr[arr.length - i - 1];
+		}
+		return newIntRay;
+	}
+	
+	private int countLessThan(int[] arr, int lessThanValue) {
+		int count = 0;
+		for(int value: arr) {
+			if(value < lessThanValue) {
+				count++;
+			}
+		}
+		return count;
+	}
+	
+	/**
+	 * Remove the element at index zero, push every other element up by 1
+	 * @param arr
+	 */
+	public void frontToBack(int[] arr) {
+		int placeHolder = arr[0];
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = arr[i + 1];
+		}
+		arr[arr.length - 1] = placeHolder;
+	}
+	
+	/**
+	 * Moves the front to the back repeatedly, exactly n times
+	 * @param arr
+	 * @param n
+	 */
+	public void cycleThrough(int[] arr, int n) {
+		for(int i = 0; i < n; i++) {
+			frontToBack(arr);
+		}
+	}
+	
+	/**
+	 * Returns the length of the longest sequence of consecutive integers in array
+	 * @param arr
+	 * @return
+	 */
+	public int[] longestConsecutiveSequence(int[] arr) {
+		int highestCount = 0;
+		int position = 0; 
+		for(int i = 0; i < arr.length; i++) {
+			if(consecutiveLength(arr, i) > highestCount) {
+				highestCount = consecutiveLength(arr, i);
+				position = i;
+			}
+		}
+		int[] answer = {highestCount, position};
+		return answer;
+	}
+	
+	public int[] longestConsecSeqAndPos(int[] arr) {
+		int[] data = new int[2];
+		int[] result = longestConsecutiveSequence(arr);
+		data[0] = result[0];
+		data[1] = result[1];
+		return data;
+	}
+	
+	public int consecutiveLength(int[] arr, int pos) {
+		int count = 1;
+		for(int i = pos; i < arr.length - 1; i++) {
+			if(isNext(arr, i))
+			{
+				count++;
+			}
+			else
+			{
+				return count;
+			}
+		}
+		//This checks to see if the last element is consecutive or not as the FOR loop does not check 
+		if(isNext(arr, arr.length - 2))
+		{
+			count++;
+		}
+		return count;
+	}
+	
+	public boolean isNext(int[] arr, int pos) {
+		return arr[pos] + 1 == arr[pos + 1];
+	}
+	
+	//---------CLASSWORK 10/17/2017 --------------
+	
 	private void shuffle(int[] arr) {
 		for(int i = 0; i < arr.length; i++) {
 			int swapper1 = (int) (Math.random() * arr.length);
@@ -51,7 +149,7 @@ public class ArraysMain {
 
 	private void populate(int[] intRay) {
 		for(int i = 0; i < intRay.length; i++) {
-			intRay[i] = diceRoll(3);
+			intRay[i] = diceRoll(2);
 		}
 		//This does not work:
 		/*
