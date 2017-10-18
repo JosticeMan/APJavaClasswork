366e59402669d83b7dbd8f0622ae72ed62aa6412
BT_1N3_17 <BT_1N3_17@BT_1N3_17.bktech.local>
2017-10-18 11:34:01 -0400

    -

--- JustinYau/src/arrays/ObjectArrays.java
+++ JustinYau/src/arrays/ObjectArrays.java
@@ -18,6 +18,7 @@ public class ObjectArrays {
 			String firstName = randomNameFrom(Person.FIRST_START, Person.FIRST_MIDDLE, Person.FIRST_END);
 			String lastName = randomNameFrom(Person.LAST_START, Person.LAST_MIDDLE, Person.LAST_END);
 			Borough borough = randomBorough();
+			//In Object[] (not primitive arrays like int), you can have multiple data types) (Sub classes of the declared type)
 			if(Math.random() < .6) {
 				int grade = (int) (Math.random() * 5) + 9;
 				people[i] = new Student(firstName, lastName, borough, grade);
