e8c3b43a55436f2d7d002208929909460ccfde74
BT_1N3_17 <BT_1N3_17@BT_1N3_17.bktech.local>
2017-10-24 11:39:54 -0400

    -

--- JustinYau/src/caveExplorer/CaveExplorer.java
+++ JustinYau/src/caveExplorer/CaveExplorer.java
@@ -7,7 +7,7 @@ public class CaveExplorer {
 	public static CaveRoom[][] caves; //Every one in the cave
 	public static Scanner in; //For user input
 	public static CaveRoom currentRoom; //Changes based on how the user navigated
-	public static Inventory invetory; //Where are all objects found are kept
+	public static Inventory inventory; //Where are all objects found are kept
 	
 	public static void main(String[] args) {
 		
--- JustinYau/src/caveExplorer/CaveRoom.java
+++ JustinYau/src/caveExplorer/CaveRoom.java
@@ -62,6 +62,78 @@ public class CaveRoom {
 		return direction[dir];
 	}
 	
+	public void enter() {
+		contents = "x";
+	}
+	
+	public void leave() {
+		contents = defaultContents;
+	}
+	
+	/**
+	 * Gives this room access to anotherRoom (and vice-versa)
+	 * and sets a door between them, updating the directions
+	 * @param direction
+	 * @param anotherRoom
+	 * @param door
+	 */
+	public void setConnection(int direction, CaveRoom anotherRoom, Door door) {
+		addRoom(direction, anotherRoom, door);
+		anotherRoom.addRoom(oppositeDirection(direction), this, door);
+	}
+	
+	private int oppositeDirection(int direction) {
+		//int[] opposites = {2, 3, 0, 1};
+		//return opposites[direction];
+		return (direction + 2)%4;
+	}
+
+	public void addRoom(int direction, CaveRoom cave, Door door) {
+		borderingRooms[direction] = cave;
+		doors[direction] = door;
+		setDirections();
+	}
+	
+	public void interpretInput(String input) {
+		while(isValid(input)) {
+			System.out.println("You can only enter 'w', 'a', 's', or 'd'");
+			input = CaveExplorer.in.nextLine();
+		}
+		int direction = determineDirection(input);
+		//Task: convert user input into a direction
+		//don't use more than one
+		goToRoom(direction);
+	}
+	
+	private void goToRoom(int direction) {
+		//First, protect against null pointer exception
+		//(user cannot go through a non existent door)
+		if(borderingRooms[direction] != null && 
+				doors[direction] != null) {
+			CaveExplorer.currentRoom.leave();
+			CaveExplorer.currentRoom = borderingRooms[direction];
+			CaveExplorer.currentRoom.enter();
+			CaveExplorer.inventory.updateMap();
+		}
+	} 
+
+	/**
+	 * This will be where your group sets up all the caves
+	 * and all the connections
+	 */
+	public static void setUpCaves() {
+		
+	}
+	
+	public int determineDirection(String input) {
+		return "wdsa".indexOf(input);
+	}
+	
+	private boolean isValid(String input) {
+		String validEntries = "wdsa";
+		return validEntries.indexOf(input) > - 1 && input.length() == 1;
+	}
+
 	public String getDescription() {
 		return description;
 	}
