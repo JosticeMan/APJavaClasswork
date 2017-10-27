19e8762bce38bb01a6426baf028aa3f83a6960e7
Justin Yau <justiceman8998@gmail.com>
2017-10-27 11:54:41 -0400

    Updated for today

--- JustinYau/src/caveExplorer/CaveExplorer.java
+++ JustinYau/src/caveExplorer/CaveExplorer.java
@@ -8,9 +8,28 @@ public class CaveExplorer {
 	public static Scanner in; //For user input
 	public static CaveRoom currentRoom; //Changes based on how the user navigated
 	public static Inventory inventory; //Where are all objects found are kept
+	public static boolean playing = true;
 	
 	public static void main(String[] args) {
+		in = new Scanner(System.in);
+		CaveRoom.setUpCaves();
 		
+		inventory = new Inventory();
+		startExploring;
 	}
 	
+	public static startExploring() {
+		while(playing) {
+ 			print(inventory.getDescription());
+ 			print(currentRoom.getDescription());
+ 			print("What would you like to do?");
+ 			String input = in.nextLine();
+ 			currentRoom.interpretInput(input);
+ 		}
+	}
+	
+	public static void print(String s) {
+ 		//NOTE: later, you can replace this line with the more sophistocated "multiLinePrint" from Chatbot
+ 		System.out.println(s);
+  	}
 }
