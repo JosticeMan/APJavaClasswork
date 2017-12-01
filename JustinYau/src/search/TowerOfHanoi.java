48d1cfc84aeae8fda10398d552c64fcf6481015f
BT_1N3_17 <BT_1N3_17@BT_1N3_17.bktech.local>
2017-12-01 11:46:22 -0500

    -

---
+++ JustinYau/src/search/TowerOfHanoi.java
@@ -0,0 +1,39 @@
+package search;
+
+public class TowerOfHanoi {
+
+	private static String[] post;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int numberOfDiscs = 7;
+		for(int i = 0; i < numberOfDiscs; i++) {
+			post[i] = "";
+		}
+		hanoi(7, "a", "b", "c");
+	}
+	
+	public static void hanoi(int disc, String start, String helper, String end) {
+		if(disc == 1) {
+			moveDisk(start, end); //Move smallest disc to end post
+		}
+		else {
+			hanoi(disc - 1, start, end, helper); //Move the 2nd largest stack to the helper post
+			moveDisk(start, end); //Move largest disc to end post
+			hanoi(disc -1, helper, start, end); //Move the 2nd largest stack to the end post
+		}
+		
+	}
+	
+	public static void moveDisk(String start, String end) {
+		int startIndex = indexOfLetter(start);
+		int endIndex = indexOfLetter(end);
+		post[startIndex][post.length - 1]; 
+	}
+	
+	public static int indexOfLetter(String letter) {
+		return "abc".indexOf(letter);
+	}
+	
+	
+}
